b47446bd3cdd667ea70ac2a464b165cfb7d3ed10
helton-godoy <chileno.brasil@gmail.com>
2026-01-18 00:48:22 -0400

    fix(build-pkg)!: clean up and fix build-pkg issues

--- build-pkg/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_showbox-ui_FILE /workspace/showbox-studio/build-pkg/libs/showbox-ui/libshowbox-ui.a)
-set(__QT_DEPLOY_TARGET_showbox-ui_TYPE STATIC_LIBRARY)
-set(__QT_DEPLOY_TARGET_showbox-studio_FILE /workspace/showbox-studio/build-pkg/showbox-studio)
-set(__QT_DEPLOY_TARGET_showbox-studio_TYPE EXECUTABLE)
